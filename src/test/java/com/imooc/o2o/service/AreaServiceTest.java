114a5af31c8ccfe00d9870fe9449d524c351236e
zwyu123 <zwyu2017@163.com>
2019-07-14 22:45:47 +0800

    验证dao,service,controller

---
+++ src/test/java/com/imooc/o2o/service/AreaServiceTest.java
@@ -0,0 +1,22 @@
+package com.imooc.o2o.service;
+
+import com.imooc.o2o.BaseTest;
+import com.imooc.o2o.entity.Area;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AreaServiceTest extends BaseTest {
+
+    @Autowired
+    private AreaService areaService;
+
+    @Test
+    public void testGetAreaList() {
+        List<Area> areaList = areaService.getAreaList();
+        assertEquals("西苑",areaList.get(0).getAreaName());
+    }
+}
