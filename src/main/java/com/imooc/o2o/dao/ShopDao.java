9ac3f74c60cdad316bcb2338921758987bef77ce
zwyu123 <zwyu2017@163.com>
2019-07-18 00:51:27 +0800

    Dao层更新店铺

--- src/main/java/com/imooc/o2o/dao/ShopDao.java
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -9,4 +9,11 @@ public interface ShopDao {
      * @return
      */
     int insertShop(Shop shop);
+
+    /**
+     * 更新店铺信息
+     * @param shop
+     * @return
+     */
+    int updateShop(Shop shop);
 }
--- src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
@@ -5,6 +5,7 @@ import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.entity.PersonInfo;
 import com.imooc.o2o.entity.Shop;
 import com.imooc.o2o.entity.ShopCategory;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,6 +18,7 @@ public class ShopDaoTest extends BaseTest {
     private ShopDao shopDao;
 
     @Test
+    @Ignore
     public void testInsertShop() {
         Shop shop = new Shop();
         PersonInfo owner = new PersonInfo();
@@ -37,6 +39,17 @@ public class ShopDaoTest extends BaseTest {
         shop.setEnableStatus(1);
         shop.setAdvice("审核中");
         int effectedNum = shopDao.insertShop(shop);
+        assertEquals(1,effectedNum);
+    }
+
+    @Test
+    public void testUpdateShop() {
+        Shop shop = new Shop();
+        shop.setShopId(1L);
+        shop.setShopDesc("测试描述");
+        shop.setShopAddr("测试地址");
+        shop.setLastEditTime(new Date());
+        int effectedNum = shopDao.updateShop(shop);
         assertEquals(1,effectedNum);
     }
 }
